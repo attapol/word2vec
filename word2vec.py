a30c9f95efee48266bdd3bf2ba9e84fa88fbcab4
nozomiyamada <y.nozomi320@gmail.com>
2019-02-08 05:12:10 +0700

    update

--- word2vec.py
+++ word2vec.py
@@ -2,6 +2,8 @@
 @author: Nozomi
 """
 from gensim.models import word2vec
+from gensim.models import KeyedVectors
+
 import csv
 import random
 import numpy as np
@@ -60,12 +62,13 @@ def mahalanobis(vectors):
     mahal_dis = list(map(lambda vec: np.sqrt(np.dot(np.dot(vec, inv_matrix), vec.T)), deviation_vec))
     return mahal_dis
 
-def make_model(open_tsv='tokenized.tsv', save_name='cbow.model'):
+def make_model(open_tsv='tokenized_test.tsv', save_name='test.bin', skipgram=0):
     file = open(open_tsv, 'r', encoding='utf-8')
     lines = csv.reader(file, delimiter='\t')
     word_list = [line[1].split('\t') for line in lines]
-    model = word2vec.Word2Vec(word_list, sg=0, size=300, min_count=5, window=15)  # CBOW: sg=0, skip-gram: sg=1
-    model.save(save_name)
+    model = word2vec.Word2Vec(word_list, sg=skipgram, size=300, min_count=5, window=15)  # CBOW: sg=0, skip-gram: sg=1
+    #model.save(save_name)
+    model.wv.save_word2vec_format(save_name, binary=True)
 
 class Metonymy:
 
